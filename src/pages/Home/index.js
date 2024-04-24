7dafa14053f4d001d7fde2f94280a06651338aab
mmckeon16 <mmckeon204@gmail.com>
2024-04-24 00:48:38 -0400

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,21 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+        <p>
+          This is a website about me, Mary.
+        </p>
+        <a
+          className="git-link"
+          href="https://github.com/mmckeon16"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn about me
+        </a>
+    </div>
+  );
+}
+
+export default Home;
