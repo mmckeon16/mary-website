1566145303c53435b7ef035771d2a8e545b89aab
mmckeon16 <mmckeon204@gmail.com>
2024-04-27 00:37:40 -0400

    adding project page

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,13 +1,21 @@
 
 import Appbar from '../components/Appbar';
+import ProjectCard from '../components/ProjectCard';
+import starImg from './starImg.png';
+import tabImg from './tabImg.png';
+import mkdwnImg from './mkdwnImg.png';
+
+import './Projects.css';
 
 function Projects() {
   return (
     <div >
       <Appbar/>
-      <p>
-        I am a software engineer and I like to program.
-      </p>
+      <div className="projectsGroup">
+        <ProjectCard title="Star game" description="This is an intro to phaser game" img={starImg} url="https://github.com/mmckeon16/star-game"/>
+        <ProjectCard title="Tab project" description="This was a part of a takehome project for an interview to create accessible tabs." img={tabImg} url="https://github.com/mmckeon16/mailchimp-takehome"/>
+        <ProjectCard title="Markdown converter" description="This was a part of a takehome project for an interview to create a script that converts markdown to html." img={mkdwnImg} url="https://github.com/mmckeon16/mailchimp-takehome-take2"/>
+      </div>
     </div>
   );
 }
--- src/pages/components/ProjectCard.js
+++ src/pages/components/ProjectCard.js
@@ -1,26 +1,36 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import NavButton from './NavButton';
+import CardContent from '@mui/material/CardContent';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
+import {
+  useNavigate,
+} from "react-router-dom";
 
-function ProjectCard() {
-return (
-<Card sx={{ maxWidth: 345 }}>
-<CardMedia
-  sx={{ height: 140 }}
-  image="/static/images/cards/contemplative-reptile.jpg"
-  title="green iguana"
-/>
-<CardContent>
-  <Typography gutterBottom variant="h5" component="div">
-    Lizard
-  </Typography>
-  <Typography variant="body2" color="text.secondary">
-    Lizards are a widespread group of squamate reptiles, with over 6,000
-    species, ranging across all continents except Antarctica
-  </Typography>
-</CardContent>
-<CardActions>
-  <Button size="small">Share</Button>
-  <Button size="small">Learn More</Button>
-</CardActions>
-</Card>)};
+function ProjectCard({img, title, description, url}) {
+  const navigate = useNavigate();
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={img}
+        title={title}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={()=>{window.location.href=url}}>Learn More</Button>
+      </CardActions>
+    </Card>
+  )
+};
+
+export default ProjectCard;
